655cbedf57e12de3ed3afe736fbaa5b30c04b4e1
Sahil Yasar <sahilyasar1@gmail.com>
2026-09-12 19:57:43 +0600

    Footer initialized need to complete it

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Suspense } from "react"
 import Banner from "./components/Banner/Banner"
 import Navbar from "./components/Navbar/Navbar"
 import Technologies from "./components/Technologies/Technologies"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Suspense fallback={<div>Technologies Loading...</div>}>
+      <Suspense fallback={<div className="text-center">Technologies Loading...</div>}>
         <Technologies></Technologies>
       </Suspense>
+      <Footer></Footer>
+
     </>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container mx-auto px-4">
+                Footer
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
