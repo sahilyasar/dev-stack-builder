2d3f1f2bfc16c72e9f9d2bc8b9879c3ef299c443
Sahil Yasar <sahilyasar1@gmail.com>
2026-09-13 20:59:38 +0600

    Final website with responsive mobile nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import logotext from "../../assets/logo-text.png"
 const Navbar = () => {
     return (
-        <nav  className="sticky top-0 z-1  bg-white">
+        <nav className="sticky top-0 z-1 bg-white">
             <div className='mx-auto container flex items-center justify-between py-4 px-4'>
 
                 <div >
                     <img className="h-8 w-auto " src={logotext} alt="Dev Stack" />
                 </div>
-                <div className='flex gap-6 text-sm font-medium'>
+                <div className='hidden md:flex gap-6 text-sm font-medium'>
                     <a className="transition hover:text-pink-500 " href="#">Home</a>
                     <a className="transition hover:text-pink-500 " href="#">Technologies</a>
                     <a className="transition hover:text-pink-500 " href="#">Projects</a>
